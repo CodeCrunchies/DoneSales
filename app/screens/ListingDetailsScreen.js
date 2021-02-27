9702fddadd5656d1e841a077fc298967b6c6c7dc
Irena <irenejpopova6@gmail.com>
2021-02-27 15:36:55 +0100

    Listing

---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import Text from "../components/Text";
+
+import ListItem from "../components/ListItem";
+import colors from "../config/colors";
+
+function ListingDetailsScreen(props) {
+  return (
+    <View>
+      <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <View style={styles.detailsContainer}>
+        <Text style={styles.title}>Red Jacket for sale</Text>
+        <Text style={styles.price}>100</Text>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/irena.png")}
+            title="Irena"
+            subtitle="6 Listings"
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
+  },
+});
+
+export default ListingDetailsScreen;
