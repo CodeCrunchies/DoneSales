60b21fc5faa6df6075e1a55643360986baacfc29
Irena <irenejpopova6@gmail.com>
2021-02-27 15:37:25 +0100

    update

--- App.js
+++ App.js
@@ -1,34 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Image, SafeAreaView } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-
-  /* console.log(require("./assets/favicon.png")); */
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text> Hello</Text>
-      <TouchableOpacity onPress={() => console.log('Image tapped')}>
-
-        <Image
-          blurRadius={2}
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/id/237/200/300",
-          }} />
-      </TouchableOpacity>
-       <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
